d2d1b800fa2082c71c674e4fc7318b94418507e6
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-02-23 14:29:54 +0500

    work with logic

--- ColorizedApp-2.0/ViewControllers/PalletViewController.swift
+++ ColorizedApp-2.0/ViewControllers/PalletViewController.swift
@@ -26,9 +26,19 @@ final class PalletViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redTF.delegate = self
+        greenTF.delegate = self
+        blueTF.delegate = self
+        
         colorView.layer.cornerRadius = 15
         showStartPallet()
-       
+    }
+    
+    // Метод для скрытия клавиатуры тапом по экрану
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
     @IBAction func sliderAction(_ sender: UISlider) {
@@ -47,6 +57,7 @@ final class PalletViewController: UIViewController {
     }
     
     @IBAction func doneButtonTapped(_ sender: Any) {
+        view.endEditing(true)
     }
     
     
@@ -54,7 +65,65 @@ final class PalletViewController: UIViewController {
         delegate.setNewColor(for: color)
         dismiss(animated: true)
     }
-    private func setColor() {
+    
+}
+
+//MARK: - UITextFieldDelegate
+extension PalletViewController: UITextFieldDelegate {
+    
+   
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let newValue = textField.text else { return }
+        
+        updatTextFieldValues(newValue, textField)
+    }
+}
+
+
+//MARK: - Private Methods
+private extension PalletViewController {
+
+    
+    func updatTextFieldValues(_ newValue: String, _ textField: UITextField) {
+        if let colorValue = Float(newValue) {
+            switch textField {
+            case redTF:
+                redSlider.setValue(colorValue, animated: true)
+                color.red = redSlider.value
+                redTF.text = string(from: redSlider)
+                
+            case greenTF:
+                greenSlider.setValue(colorValue, animated: true)
+                color.green = greenSlider.value
+                greenTF.text = string(from: greenSlider)
+                
+            case blueTF:
+                blueSlider.setValue(colorValue, animated: true)
+                color.blue = blueSlider.value
+                blueTF.text = string(from: blueSlider)
+            
+            default:
+                print(" error ")
+            }
+            setColor()
+            
+        }
+        else {
+            showAlert()
+            textField.text = ""
+        }
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Ошибка", message: "Введены неправильные данные", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ок", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+        
+    }
+    
+    func setColor() {
         colorView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
@@ -63,20 +132,22 @@ final class PalletViewController: UIViewController {
         )
     }
     
-    private func setValue(for textFields: UITextField...) {
+    func setValue(for textFields: UITextField...) {
         textFields.forEach { textField in
             switch textField {
             case redTF:
                 addDoneButton(for: redTF)
                 redTF.text = string(from: redSlider)
             case greenTF:
+                addDoneButton(for: greenTF)
                 greenTF.text = string(from: greenSlider)
             default:
+                addDoneButton(for: blueTF)
                 blueTF.text = string(from: blueSlider)
             }
         }
     }
-    private func showStartPallet() {
+    func showStartPallet() {
         redSlider.setValue(color.red, animated: false)
         greenSlider.setValue(color.green, animated: false)
         blueSlider.setValue(color.blue, animated: false)
@@ -84,12 +155,10 @@ final class PalletViewController: UIViewController {
         setValue(for: redTF, greenTF, blueTF)
     }
     
-    private func string(from slider: UISlider) -> String {
+    func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
-}
-
-extension PalletViewController {
+    
     func addDoneButton(for textField: UITextField) {
         let toolbar = UIToolbar(
             frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
